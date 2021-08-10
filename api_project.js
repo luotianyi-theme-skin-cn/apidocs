bc0de718acfcb6aaafb4e2b325b5f8328dcf09f3
YuxiangWang0525 <66881174+YuxiangWang0525@users.noreply.github.com>
2021-08-10 22:32:38 +0800

    ins

---
+++ api_project.js
@@ -0,0 +1,23 @@
+define({
+  "name": "洛天依主题皮肤站中国站Api开发文档",
+  "version": "0.0.1",
+  "description": "Powered By Blessing-Skin-Server WebAPI",
+  "title": "apidoc接口",
+  "url": "https://luotianyi-theme-skin.top",
+  "order": [
+    "Example",
+    "Home"
+  ],
+  "template": {
+    "forceLanguage": "zh_cn"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-08-10T14:32:13.796Z",
+    "url": "https://apidocjs.com",
+    "version": "0.29.0"
+  }
+});
